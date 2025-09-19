813046524ca1dd8612b818caddc247ce890fa1b6
EmsinaDR <emsinadr@gmail.com>
2025-09-19 21:28:46 +0700

    Hapus semua isi repo

--- resources/views/components/create-layout-modal.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="modal fade" id="CreateKaldik" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-
-    <div class="modal-dialog">
-        <div class="modal-content">
