4da115e0dd284be96cb8b5ff0860c9c605b734eb
EmsinaDR <emsinadr@gmail.com>
2025-10-25 21:30:25 +0700

    Hapus semua isi repo

--- routes/program/bukutamu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('program/bukutamu')->group(function () {
-    // Tambahkan route modul di sini
-});
--- routes/program/kelulusan.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('program/kelulusan')->group(function () {
-    // Tambahkan route modul di sini
-});
