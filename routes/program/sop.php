c084c37a300bc528f261b933e22676184ab7583e
EmsinaDR <emsinadr@gmail.com>
2025-10-30 21:30:29 +0700

    Hapus semua isi repo

--- routes/program/sop.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('program/sop')->group(function () {
-    // SOP
-    Route::resource('data-sop', App\Http\Controllers\Program\SOP\DataSOPController::class);
-});
