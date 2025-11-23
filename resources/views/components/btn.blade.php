8fe62cc93ed9ea4b85fdc8ae75cf935ad63e2ec7
EmsinaDR <emsinadr@gmail.com>
2025-11-23 21:30:33 +0700

    Hapus semua isi repo

--- resources/views/components/btn.blade.php
+++
@@ -1,9 +0,0 @@
-@php
-$data = explode('/', $slot);
-
-@endphp
-{{-- submit/Save/fa fa-save/btn btn-default bg-primary float-right --}}
-
-
-{{-- submit / Nama Btn / ICON / Kelas / JSFUnction --}}
-<button type="{{ $data[0] }}" class="btn {{ $data[3] }} mt-4"><i class="{{ $data[2] }}"></i> {{ $data[1] }}</button>
--- routes/program/prestasi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('program')->group(function () {
-    // Tambahkan route modul di sini
-    Route::resource('prestasi', App\Http\Controllers\Program\Prestasi\DataPrestasiController::class)->middleware(['auth', 'verified']);
-});
